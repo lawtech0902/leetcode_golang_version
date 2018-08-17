8b00fc7a68ae196daac5909cefa74e8cccfcb4c8
Lawtech <584563542@qq.com>
2018-08-18 01:29:20 +0800

    019_remove_nth_node_from_end_of_list

---
+++ 019/019_remove_nth_node_from_end_of_list.go
@@ -0,0 +1,45 @@
+package _19
+
+/*
+__author__ = 'lawtech'
+__date__ = '2018/8/18 上午1:18'
+*/
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func removeNthFromEnd(head *ListNode, n int) *ListNode {
+	// 快慢指针
+	if head == nil {
+		return head
+	}
+
+	fast, i := head, 0
+	for ; i < n; i++ {
+		if fast == nil {
+			break
+		}
+		fast = fast.Next
+	}
+
+	// 链表长度小于n
+	if i < n {
+		return head
+	}
+
+	if fast == nil {
+		return head.Next
+	}
+
+	slow := head
+	for fast.Next != nil {
+		slow = slow.Next
+		fast = fast.Next
+	}
+
+	slow.Next = slow.Next.Next
+
+	return head
+}
