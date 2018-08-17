3e441dcd684c0713a18d5e2b9976c71d5b5c1792
Lawtech <584563542@qq.com>
2018-08-18 00:45:58 +0800

    009_palindrome_number

--- 009/009_palindrome_number.go
+++ 009/009_palindrome_number.go
@@ -1,6 +1,26 @@
-package _09
+package main
+
+import (
+	"strconv"
+)
 
 /*
 __author__ = 'lawtech'
 __date__ = '2018/8/18 上午12:40'
 */
+
+func isPalindrome(x int) interface{} {
+	if x < 0 {
+		return false
+	}
+
+	s := strconv.Itoa(x)
+
+	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
+		if s[i] != s[j] {
+			return false
+		}
+	}
+
+	return true
+}
