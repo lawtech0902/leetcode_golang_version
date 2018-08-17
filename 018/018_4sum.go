762461a5b5147d17caf2ce642e44835c7c32b309
Lawtech <584563542@qq.com>
2018-08-18 01:17:22 +0800

    018_4_sum

---
+++ 018/018_4sum.go
@@ -0,0 +1,63 @@
+package _18
+
+import "sort"
+
+/*
+__author__ = 'lawtech'
+__date__ = '2018/8/18 上午1:11'
+*/
+
+func fourSum(nums []int, target int) [][]int {
+	sort.Ints(nums)
+	res := [][]int{}
+
+	for i := 0; i < len(nums)-3; i++ {
+		// 避免添加重复的结果
+		// i>0 是为了防止nums[i-1]溢出
+		if i > 0 && nums[i] == nums[i-1] {
+			continue
+		}
+
+		for j := i + 1; j < len(nums)-2; j++ {
+			// 避免添加重复的结果
+			// nums[j-1]虽然不会溢出
+			// 但是不添加 j > i+1 的话，会漏掉那些i和j为相同数值的答案
+			if j > i+1 && nums[j] == nums[j-1] {
+				continue
+			}
+
+			l, r := j+1, len(nums)-1
+			for l < r {
+				s := nums[i] + nums[j] + nums[l] + nums[r]
+				switch {
+				case s > target:
+					r --
+				case s < target:
+					l ++
+				default:
+					res = append(res, []int{nums[i], nums[j], nums[l], nums[r]})
+					l, r = next(nums, l, r)
+				}
+			}
+		}
+	}
+
+	return res
+}
+
+func next(nums []int, l, r int) (int, int) {
+	for l < r {
+		switch {
+		case nums[l] == nums[l+1]:
+			l++
+		case nums[r] == nums[r-1]:
+			r--
+		default:
+			l++
+			r--
+			return l, r
+		}
+	}
+
+	return l, r
+}
