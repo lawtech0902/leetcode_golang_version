343e586b8666c34068b66a0dcc574567067715ca
Lawtech <584563542@qq.com>
2018-08-18 23:00:42 +0800

    344_reverse_string

--- 344/344_reverse_string.go
+++ 344/344_reverse_string.go
@@ -1,6 +1,20 @@
-package _44
+package _344
 
 /*
 __author__ = 'lawtech'
 __date__ = '2018/8/18 下午10:00'
 */
+
+func reverseString(s string) string {
+	bytes := []byte(s)
+
+	i, j := 0, len(s)-1
+
+	for i < j {
+		bytes[i], bytes[j] = bytes[j], bytes[i]
+		i ++
+		j --
+	}
+
+	return string(bytes)
+}
