bc82f0c111c4e507aa34229bc9ba197c785bc80d
Lawtech <584563542@qq.com>
2018-08-18 20:24:50 +0800

    链表、树节点结构定义

---
+++ 024/024_swap_nodes_in_pairs.go
@@ -0,0 +1,6 @@
+package _24
+
+/*
+__author__ = 'lawtech'
+__date__ = '2018/8/18 下午8:22'
+*/
---
+++ types.go
@@ -0,0 +1,19 @@
+package leetcode_golang_version
+
+/*
+__author__ = 'lawtech'
+__date__ = '2018/8/18 下午8:22'
+*/
+
+// 链表
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+// 树
+type TreeNode struct {
+	Val   int
+	Left  TreeNode
+	Right TreeNode
+}
