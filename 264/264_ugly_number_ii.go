9b1a805cbd0f9ebe32d66a85ffd7f897aa80efdc
Lawtech <584563542@qq.com>
2018-08-19 23:30:55 +0800

    264_ugly_number_ii

--- 264/264_ugly_number_ii.go
+++ 264/264_ugly_number_ii.go
@@ -1,6 +1,36 @@
-package _64
+package _264
+
+import (
+	"go_projects/leetcode_golang_version"
+)
 
 /*
 __author__ = 'lawtech'
 __date__ = '2018/8/19 下午11:22'
 */
+
+func nthUglyNumber(n int) int {
+	if n == 0 {
+		return 0
+	}
+
+	q := []int{1}
+	i2, i3, i5 := 0, 0, 0
+
+	for len(q) < n {
+		m2, m3, m5 := q[i2]*2, q[i3]*3, q[i5]*5
+		m, _ := leetcode_golang_version.MinMax([]int{m2, m3, m5})
+		switch m {
+		case 2:
+			i2 += 1
+		case 3:
+			i3 += 1
+		case 5:
+			i5 += 1
+		default:
+		}
+		q = append(q, m)
+	}
+
+	return q[len(q)-1]
+}
