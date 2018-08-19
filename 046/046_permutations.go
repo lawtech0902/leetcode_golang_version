36bdc1774d89185ff44adb80636943a94d18daa5
Lawtech <584563542@qq.com>
2018-08-19 21:59:10 +0800

    046_permutations

---
+++ 046/046_permutations.go
@@ -0,0 +1,36 @@
+package _46
+
+/*
+__author__ = 'lawtech'
+__date__ = '2018/8/19 下午5:34'
+*/
+
+func permute(nums []int) [][]int {
+	if len(nums) == 0 {
+		return [][]int{}
+	}
+	var res [][]int
+	doPermute(&res, []int{}, nums)
+	return res
+}
+
+func doPermute(res *[][]int, currentNums []int, remainingNums []int) {
+	rlen := len(remainingNums)
+	if rlen == 0 {
+		*res = append(*res, currentNums)
+		return
+	}
+
+	for i := 0; i < rlen; i++ {
+		newRemaining := make([]int, rlen-1)
+		copy(newRemaining[:i], remainingNums[:i])
+		copy(newRemaining[i:], remainingNums[i+1:])
+
+		newCurrent := make([]int, len(currentNums)+1)
+		copy(newCurrent[:len(currentNums)], currentNums)
+		newCurrent[len(currentNums)] = remainingNums[i]
+
+		doPermute(res, newCurrent, newRemaining)
+	}
+}
+
