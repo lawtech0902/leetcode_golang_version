802517e3303fa62e825549f59f0d58b2447be07e
Lawtech <584563542@qq.com>
2018-08-21 11:54:45 +0800

    322_coin_change

--- 322/322_coin_change.go
+++ 322/322_coin_change.go
@@ -5,6 +5,37 @@ __author__ = 'lawtech'
 __date__ = '2018/8/21 上午11:31'
 */
 
+/*
+if dp[x+c] < 0 || dp[x+c] > dp[x]+1 {
+	dp[x+c] = dp[x] + 1
+}
+其中dp[x]代表凑齐金额x所需的最少硬币数，c为可用的硬币面值
+
+初始令dp[0] = 0
+*/
+
 func coinChange(coins []int, amount int) int {
-	
+	// 傻逼一样的数组声明
+	dp := make([]int, amount+1)
+	dp[0] = 0
+	for i := 1; i < len(dp); i++ {
+		dp[i] = -1
+	}
+
+	for x := 0; x < amount; x++ {
+		if dp[x] < 0 {
+			continue
+		}
+		for _, c := range coins {
+			if x+c > amount {
+				continue
+			}
+
+			if dp[x+c] < 0 || dp[x+c] > dp[x]+1 {
+				dp[x+c] = dp[x] + 1
+			}
+		}
+	}
+
+	return dp[amount]
 }
