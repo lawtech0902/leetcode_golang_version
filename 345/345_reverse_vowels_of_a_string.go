b5d8bfe6abe4d94849737d8ad37dd649913fe680
Lawtech <584563542@qq.com>
2018-08-24 19:13:29 +0800

    345_reverse_vowels_of_a_string

---
+++ 345/345_reverse_vowels_of_a_string.go
@@ -0,0 +1,35 @@
+package _345
+
+/*
+__author__ = 'lawtech'
+__date__ = '2018/8/24 下午6:07'
+*/
+
+func reverseVowels(s string) string {
+	bytes := []byte(s)
+	i, j := 0, len(s)-1
+
+	for {
+		for i < len(s) && !isVowel(bytes[i]) {
+			i++
+		}
+
+		for j >= 0 && !isVowel(bytes[j]) {
+			j--
+		}
+
+		if i >= j {
+			break
+		}
+
+		bytes[i], bytes[j] = bytes[j], bytes[i]
+		i++
+		j--
+	}
+
+	return string(bytes)
+}
+
+func isVowel(b byte) bool {
+	return b == 'a' || b == 'e' || b == 'i' || b == 'o' || b == 'u' || b == 'A' || b == 'E' || b == 'I' || b == 'O' || b == 'U'
+}
---
+++ 633/633_sum_of_square_numbers.go
@@ -0,0 +1,6 @@
+package _33
+
+/*
+__author__ = 'lawtech'
+__date__ = '2018/8/24 下午6:03'
+*/
