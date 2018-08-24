96419997d7d141b3a9048531f3a55223f10f79cd
Lawtech <584563542@qq.com>
2018-08-24 19:14:03 +0800

    633_sum_of_square_numbers

--- 633/633_sum_of_square_numbers.go
+++ 633/633_sum_of_square_numbers.go
@@ -1,6 +1,25 @@
-package _33
+package _633
+
+import "math"
 
 /*
 __author__ = 'lawtech'
 __date__ = '2018/8/24 下午6:03'
 */
+
+func judgeSquareSum(c int) bool {
+	i, j := 0, int(math.Sqrt(float64(c)))
+
+	for i <= j {
+		powSum := i*i + j*j
+		if powSum == c {
+			return true
+		} else if powSum > c {
+			j--
+		} else {
+			i++
+		}
+	}
+
+	return false
+}
