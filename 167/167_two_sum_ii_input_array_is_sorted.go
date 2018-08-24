8beccba72f7d707cc511d94df74b3c50c97a2efa
Lawtech <584563542@qq.com>
2018-08-24 18:03:08 +0800

    167_two_sum_ii_input_array_is_sorted

---
+++ 167/167_two_sum_ii_input_array_is_sorted.go
@@ -0,0 +1,23 @@
+package _167
+
+/*
+__author__ = 'lawtech'
+__date__ = '2018/8/24 下午5:57'
+*/
+
+func twoSum(numbers []int, target int) []int {
+	l, r := 0, len(numbers)-1
+
+	for l < r {
+		s := numbers[l] + numbers[r]
+		if s == target {
+			return []int{l + 1, r + 1}
+		} else if s < target {
+			l++
+		} else {
+			r--
+		}
+	}
+
+	return []int{}
+}
