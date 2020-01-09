19c37b39cb6ffdba0ed7278db775763a01645b6c
lawtech0902 <584563542@qq.com>
2020-01-09 17:23:11 +0800

    015 3sum

--- leetcode/015/015_3sum.go
+++ leetcode/015/015_3sum.go
@@ -1,57 +1,48 @@
-package _15
-
-import "sort"
-
 /*
 __author__ = 'lawtech'
 __date__ = '2018/8/18 上午12:57'
 */
 
-func threeSum(nums []int) [][]int {
-	// 排序后，可以按规律查找
-	sort.Ints(nums)
-	res := [][]int{}
+package _15
 
-	for i := range nums {
-		// 避免添加重复的结果
-		// i>0 是为了防止nums[i-1]溢出
+import "sort"
+
+func threeSum(nums []int) [][]int {
+	var res [][]int
+
+	sort.Ints(nums)
+	size := len(nums)
+
+	if size <= 2 {
+		return res
+	}
+
+	for i := range nums[:size-2] {
 		if i > 0 && nums[i] == nums[i-1] {
 			continue
 		}
 
-		l, r := i+1, len(nums)-1
+		target := nums[i] * -1
+		left, right := i+1, size-1
+		for left < right {
+			if nums[left]+nums[right] == target {
+				res = append(res, []int{nums[i], nums[left], nums[right]})
+				right -= 1
+				left += 1
 
-		for l < r {
-			s := nums[i] + nums[l] + nums[r]
-			switch {
-			case s > 0:
-				r --
-			case s < 0:
-				l ++
-			default:
-				res = append(res, []int{nums[i], nums[l], nums[r]})
-				// 为避免重复添加，l 和 r 都需要移动到不同的元素上。
-				l, r = next(nums, l, r)
+				for left < right && nums[left] == nums[left-1] {
+					left += 1
+				}
+				for left < right && nums[right] == nums[right+1] {
+					right -= 1
+				}
+			} else if nums[left]+nums[right] > target {
+				right -= 1
+			} else {
+				left += 1
 			}
 		}
 	}
 
 	return res
-}
-
-func next(nums []int, l, r int) (int, int) {
-	for l < r {
-		switch {
-		case nums[l] == nums[l+1]:
-			l++
-		case nums[r] == nums[r-1]:
-			r--
-		default:
-			l++
-			r--
-			return l, r
-		}
-	}
-
-	return l, r
 }
