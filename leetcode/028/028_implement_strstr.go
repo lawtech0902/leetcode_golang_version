e5f97a1e60ac15bf85e8a06d67354504e121a96d
lawtech0902 <584563542@qq.com>
2020-01-10 14:33:40 +0800

    028_implement_strstr

--- leetcode/028/028_implement_strstr.go
+++ leetcode/028/028_implement_strstr.go
@@ -1,9 +1,9 @@
-package _28
-
 /*
 __author__ = 'lawtech'
 __date__ = '2018/8/18 下午8:57'
 */
+
+package _28
 
 func strStr(haystack string, needle string) int {
 	hLen, nLen := len(haystack), len(needle)
