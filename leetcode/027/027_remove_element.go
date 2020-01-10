376f01039af1deeff957a7937f5f924d20045a9b
lawtech0902 <584563542@qq.com>
2020-01-10 14:32:28 +0800

    027_remove_element

--- leetcode/027/027_remove_element.go
+++ leetcode/027/027_remove_element.go
@@ -1,19 +1,20 @@
-package _27
-
 /*
 __author__ = 'lawtech'
 __date__ = '2018/8/18 下午8:51'
 */
 
+package _27
+
 func removeElement(nums []int, val int) int {
-	j := 0
-	for _, num := range nums {
-		if num == val {
+	cur := 0
+	for i := 0; i < len(nums); i++ {
+		if nums[i] == val {
 			continue
 		} else {
-			nums[j] = num
-			j++
+			nums[cur] = nums[i]
+			cur += 1
 		}
 	}
-	return j
+
+	return cur
 }
