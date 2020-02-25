7f040b00d6512dc2fd439191263934a4e5c3c8ab
lawtech0902 <584563542@qq.com>
2020-02-25 16:39:02 +0800

    109_convert_sorted_list_to_binary_search_tree

--- leetcode/109/109_convert_sorted_list_to_binary_search_tree.go
+++ leetcode/109/109_convert_sorted_list_to_binary_search_tree.go
@@ -1,14 +1,20 @@
-package _109
-
-import "go_projects/leetcode_golang_version"
-
 /*
 __author__ = 'lawtech'
 __date__ = '2018/8/19 下午5:12'
 */
 
-type ListNode = leetcode_golang_version.ListNode
-type TreeNode = leetcode_golang_version.TreeNode
+package _109
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
 
 func sortedListToBST(head *ListNode) *TreeNode {
 	var nums []int
@@ -19,14 +25,16 @@ func sortedListToBST(head *ListNode) *TreeNode {
 		p = p.Next
 	}
 
-	return sortedArrToBST(nums)
+	return sortedArrToBST()
 }
 
 func sortedArrToBST(nums []int) *TreeNode {
 	size := len(nums)
+
 	if size == 0 {
 		return nil
 	}
+
 	if size == 1 {
 		return &TreeNode{Val: nums[0]}
 	}
@@ -34,5 +42,6 @@ func sortedArrToBST(nums []int) *TreeNode {
 	root := &TreeNode{Val: nums[size/2]}
 	root.Left = sortedArrToBST(nums[:size/2])
 	root.Right = sortedArrToBST(nums[size/2+1:])
+
 	return root
 }
