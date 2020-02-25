b83d11e480be49ea0ef8933fd3a6c79b2adee8e2
lawtech0902 <584563542@qq.com>
2020-02-25 16:07:44 +0800

    107_binary_tree_level_order_traversal_ii

--- leetcode/108/108_convert_sorted_array_to_binary_search_tree.go
+++ leetcode/108/108_convert_sorted_array_to_binary_search_tree.go
@@ -1,20 +1,22 @@
-package _108
-
-import "go_projects/leetcode_golang_version"
-
 /*
 __author__ = 'lawtech'
 __date__ = '2018/8/19 下午5:23'
 */
 
-type ListNode = leetcode_golang_version.ListNode
-type TreeNode = leetcode_golang_version.TreeNode
+package _108
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
 
 func sortedArrayToBST(nums []int) *TreeNode {
 	size := len(nums)
 	if size == 0 {
 		return nil
 	}
+
 	if size == 1 {
 		return &TreeNode{Val: nums[0]}
 	}
@@ -22,5 +24,6 @@ func sortedArrayToBST(nums []int) *TreeNode {
 	root := &TreeNode{Val: nums[size/2]}
 	root.Left = sortedArrayToBST(nums[:size/2])
 	root.Right = sortedArrayToBST(nums[size/2+1:])
+
 	return root
 }
