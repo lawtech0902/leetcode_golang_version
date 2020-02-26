1620a0d896bf79ace5045ed85d332134f87dcdf0
lawtech0902 <584563542@qq.com>
2020-02-26 17:09:55 +0800

    114_flatten_binary_tree_to_linked_list

--- leetcode/114/114_flatten_binary_tree_to_linked_list.go
+++ leetcode/114/114_flatten_binary_tree_to_linked_list.go
@@ -1,13 +1,15 @@
-package _114
-
-import "go_projects/leetcode_golang_version"
-
 /*
 __author__ = 'lawtech'
 __date__ = '2018/8/19 下午5:25'
 */
 
-type TreeNode = leetcode_golang_version.TreeNode
+package _114
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
 
 func flatten(root *TreeNode) {
 	if root == nil {
@@ -21,6 +23,7 @@ func flatten(root *TreeNode) {
 	if p.Left == nil {
 		return
 	}
+
 	p = p.Left
 	for p.Right != nil {
 		p = p.Right
