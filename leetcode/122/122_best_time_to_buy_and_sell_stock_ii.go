33154d524e3d9dc85cdf4d954f23c6170f64d6bc
lawtech0902 <584563542@qq.com>
2020-03-02 21:13:20 +0800

    122_best_time_to_buy_and_sell_stock_ii

--- leetcode/122/122_best_time_to_buy_and_sell_stock_ii.go
+++ leetcode/122/122_best_time_to_buy_and_sell_stock_ii.go
@@ -1,18 +1,21 @@
-package _122
-
 /*
 __author__ = 'lawtech'
 __date__ = '2018/8/20 下午11:30'
 */
 
-func maxProfit(prices []int) int {
-	profit := 0
+package _122
 
+func maxProfit(prices []int) int {
+	if len(prices) <= 1 {
+		return 0
+	}
+
+	maxProfit := 0
 	for i := 1; i < len(prices); i++ {
 		if prices[i] > prices[i-1] {
-			profit += prices[i] - prices[i-1]
+			maxProfit += prices[i] - prices[i-1]
 		}
 	}
 
-	return profit
+	return maxProfit
 }
