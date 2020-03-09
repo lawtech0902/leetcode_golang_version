8174b0d63ee14c8450d72f1fd8378df314d78b3a
lawtech0902 <584563542@qq.com>
2020-03-09 14:57:24 +0800

    144_binary_tree_preorder_traversal

--- leetcode/144/144_binary_tree_preorder_traversal.go
+++ leetcode/144/144_binary_tree_preorder_traversal.go
@@ -1,33 +1,55 @@
-package _144
-
-import "go_projects/leetcode_golang_version"
-
 /*
 __author__ = 'lawtech'
 __date__ = '2018/8/18 下午10:46'
 */
 
-type TreeNode = leetcode_golang_version.TreeNode
+package _144
 
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+// 递归
 func preorderTraversal(root *TreeNode) []int {
-	// 迭代
-	stack := []*TreeNode{root}
-	var res []int
+	var (
+		res []int
+		dfs func(root *TreeNode)
+	)
 
-	for len(stack) > 0 {
-		node := stack[len(stack)-1]
-		stack = stack[:len(stack)-1]
-
-		if node != nil {
-			res = append(res, node.Val)
-			if node.Right != nil {
-				stack = append(stack, node.Right)
-			}
-			if node.Left != nil {
-				stack = append(stack, node.Left)
-			}
+	dfs = func(root *TreeNode) {
+		if root != nil {
+			res = append(res, root.Val)
+			dfs(root.Left)
+			dfs(root.Right)
 		}
 	}
 
+	dfs(root)
 	return res
 }
+
+// 迭代
+//func preorderTraversal(root *TreeNode) []int {
+//	stack := []*TreeNode{root}
+//	var res []int
+//
+//	for len(stack) > 0 {
+//		node := stack[len(stack)-1]
+//		stack = stack[:len(stack)-1]
+//
+//		if node != nil {
+//			res = append(res, node.Val)
+//			if node.Right != nil {
+//				stack = append(stack, node.Right)
+//			}
+//
+//			if node.Left != nil {
+//				stack = append(stack, node.Left)
+//			}
+//		}
+//	}
+//
+//	return res
+//}
