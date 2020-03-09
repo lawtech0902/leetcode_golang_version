b3b3519f90b24bb3a0d8b439e579ab6bdb337483
lawtech0902 <584563542@qq.com>
2020-03-09 14:05:58 +0800

    142_linked_list_cycle_ii

--- leetcode/142/142_linked_list_cycle_ii.go
+++ leetcode/142/142_linked_list_cycle_ii.go
@@ -1,39 +1,35 @@
-package _142
-
-import "go_projects/leetcode_golang_version"
-
 /*
 __author__ = 'lawtech'
 __date__ = '2018/8/19 上午2:08'
 */
 
-type ListNode = leetcode_golang_version.ListNode
+package _142
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
 
 func detectCycle(head *ListNode) *ListNode {
 	if head == nil || head.Next == nil {
 		return nil
 	}
 
-	slow := head
-	fast := head
-
-	for fast != nil && fast.Next != nil {
+	slow, fast := head.Next, head.Next.Next
+	for fast != nil && fast.Next != nil && slow != fast {
 		slow = slow.Next
 		fast = fast.Next.Next
-
-		if slow == fast {
-			break
-		}
 	}
 
-	if slow == fast {
-		slow = head
-		for slow != fast {
-			slow = slow.Next
-			fast = fast.Next
-		}
-		return slow
+	if fast == nil || fast.Next == nil {
+		return nil
 	}
 
-	return nil
+	slow = head
+	for slow != fast {
+		slow = slow.Next
+		fast = fast.Next
+	}
+
+	return slow
 }
