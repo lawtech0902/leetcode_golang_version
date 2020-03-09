20122ecd7f2296596ef67e8e8217c6ee8ee78416
lawtech0902 <584563542@qq.com>
2020-03-09 14:05:45 +0800

    141_linked_list_cycle

--- leetcode/141/141_linked_list_cycle.go
+++ leetcode/141/141_linked_list_cycle.go
@@ -1,22 +1,20 @@
-package _141
-
-import "go_projects/leetcode_golang_version"
-
 /*
 __author__ = 'lawtech'
 __date__ = '2018/8/19 上午2:04'
 */
 
-type ListNode = leetcode_golang_version.ListNode
+package _141
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
 
 func hasCycle(head *ListNode) bool {
-	slow := head
-	fast := head
-
+	slow, fast := head, head
 	for fast != nil && fast.Next != nil {
 		slow = slow.Next
 		fast = fast.Next.Next
-
 		if slow == fast {
 			return true
 		}
