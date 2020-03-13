c6554fe07b4535e3a52321aa23561d888aff9d63
lawtech0902 <584563542@qq.com>
2020-03-13 16:11:43 +0800

    161_find_peak_element

---
+++ leetcode/161/161_find_peak_element.go
@@ -0,0 +1,20 @@
+/*
+__author__ = 'lawtech'
+__date__ = '2020/03/12 6:28 下午'
+*/
+
+package _161
+
+func findPeakElement(nums []int) int {
+	l, r := 0, len(nums)-1
+	for l < r {
+		mid := (l + r) / 2
+		if nums[mid] > nums[mid+1] {
+			r = mid
+		} else {
+			l = mid + 1
+		}
+	}
+
+	return l
+}
