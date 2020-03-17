7271c745a98a0f2582d4fe25320df107565185a5
lawtech0902 <584563542@qq.com>
2020-03-17 09:41:41 +0800

    169_majority_element

--- leetcode/169/169_majority_element.go
+++ leetcode/169/169_majority_element.go
@@ -1,16 +1,17 @@
-package _169
-
 /*
 __author__ = 'lawtech'
 __date__ = '2018/8/19 下午10:00'
 */
+
+package _169
 
 func majorityElement(nums []int) int {
 	candidate, count := 0, 0
 
 	for _, num := range nums {
 		if count == 0 {
-			candidate, count = num, 1
+			candidate = num
+			count = 1
 		} else if num == candidate {
 			count += 1
 		} else {
