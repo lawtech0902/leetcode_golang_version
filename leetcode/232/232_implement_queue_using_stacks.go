4b98c7b5d14704ccd5ac77cc535b5f1757a47e2a
lawtech0902 <584563542@qq.com>
2020-04-08 15:10:48 +0800

    232_implement_queue_using_stacks

--- leetcode/232/232_implement_queue_using_stacks.go
+++ leetcode/232/232_implement_queue_using_stacks.go
@@ -1,55 +1,57 @@
-package _232
-
 /*
 __author__ = 'lawtech'
 __date__ = '2018/8/19 上午1:39'
 */
 
-type MyQueue struct {
-	inStack, outStack []int
-}
+package _232
 
-/** Initialize your data structure here. */
-func Constructor() MyQueue {
-	return MyQueue{
-		inStack:  []int{},
-		outStack: []int{},
-	}
-}
-
-/** Push element x to the back of queue. */
-func (this *MyQueue) Push(x int) {
-	this.inStack = append(this.inStack, x)
-}
-
-/** Removes the element from in front of queue and returns that element. */
-func (this *MyQueue) Pop() int {
-	this.Move()
-	popVal := this.outStack[len(this.outStack)-1]
-	this.outStack = this.outStack[:len(this.outStack)-1]
-	return popVal
-}
-
-/** Get the front element. */
-func (this *MyQueue) Peek() int {
-	this.Move()
-	return this.outStack[len(this.outStack)-1]
-}
-
-/** move the element from inStack to outStack */
-func (this *MyQueue) Move() {
-	if len(this.outStack) == 0 {
-		for len(this.inStack) > 0 {
-			this.outStack = append(this.outStack, this.inStack[len(this.inStack)-1])
-			this.inStack = this.inStack[:len(this.inStack)-1]
-		}
-	}
-}
-
-/** Returns whether the queue is empty. */
-func (this *MyQueue) Empty() bool {
-	return len(this.inStack) == 0 && len(this.outStack) == 0
-}
+// two stack
+//type MyQueue struct {
+//	inStack  []int
+//	outStack []int
+//}
+//
+///** Initialize your data structure here. */
+//func Constructor() MyQueue {
+//	return MyQueue{
+//		inStack:  []int{},
+//		outStack: []int{},
+//	}
+//}
+//
+///** Push element x to the back of queue. */
+//func (this *MyQueue) Push(x int) {
+//	this.inStack = append(this.inStack, x)
+//}
+//
+///** Removes the element from in front of queue and returns that element. */
+//func (this *MyQueue) Pop() int {
+//	this.Move()
+//	popVal := this.outStack[len(this.outStack)-1]
+//	this.outStack = this.outStack[:len(this.outStack)-1]
+//	return popVal
+//}
+//
+///** Get the front element. */
+//func (this *MyQueue) Peek() int {
+//	this.Move()
+//	return this.outStack[len(this.outStack)-1]
+//}
+//
+///** Returns whether the queue is empty. */
+//func (this *MyQueue) Empty() bool {
+//	return len(this.inStack) == 0 && len(this.outStack) == 0
+//}
+//
+///** move the element from inStack to outStack */
+//func (this *MyQueue) Move() {
+//	if len(this.outStack) == 0 {
+//		for len(this.inStack) > 0 {
+//			this.outStack = append(this.outStack, this.inStack[len(this.inStack)-1])
+//			this.inStack = this.inStack[:len(this.inStack)-1]
+//		}
+//	}
+//}
 
 /**
  * Your MyQueue object will be instantiated and called as such:
@@ -59,3 +61,43 @@ func (this *MyQueue) Empty() bool {
  * param_3 := obj.Peek();
  * param_4 := obj.Empty();
  */
+
+// one slice
+type MyQueue struct {
+	arr []int
+}
+
+/** Initialize your data structure here. */
+func Constructor() MyQueue {
+	return MyQueue{}
+}
+
+/** Push element x to the back of queue. */
+func (this *MyQueue) Push(x int) {
+	this.arr = append(this.arr, x)
+}
+
+/** Removes the element from in front of queue and returns that element. */
+func (this *MyQueue) Pop() int {
+	if this.Empty() {
+		return 0
+	}
+
+	top := this.Peek()
+	this.arr = this.arr[1:len(this.arr)]
+	return top
+}
+
+/** Get the front element. */
+func (this *MyQueue) Peek() int {
+	if this.Empty() {
+		return 0
+	}
+
+	return this.arr[0]
+}
+
+/** Returns whether the queue is empty. */
+func (this *MyQueue) Empty() bool {
+	return len(this.arr) == 0
+}
