a88be40d4f762110458f450ad4714d98173e007f
lawtech0902 <584563542@qq.com>
2020-04-13 10:53:24 +0800

    236_lowest_common_ancestor_of_a_binary_tree

---
+++ leetcode/236/236_lowest_common_ancestor_of_a_binary_tree.go
@@ -0,0 +1,34 @@
+/*
+__author__ = 'lawtech'
+__date__ = '2020/04/13 10:26 上午'
+*/
+
+package _236
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+	if root == nil {
+		return nil
+	}
+
+	if root == p || root == q {
+		return root
+	}
+
+	left := lowestCommonAncestor(root.Left, p, q)
+	right := lowestCommonAncestor(root.Right, p, q)
+	if left != nil && right != nil {
+		return root
+	}
+
+	if left == nil {
+		return right
+	}
+
+	return left
+}
