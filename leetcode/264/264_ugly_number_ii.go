362c718446cb4f44eb9c15bcc55f410d9127235f
lawtech0902 <584563542@qq.com>
2020-04-14 17:27:58 +0800

    264_ugly_number_ii

--- leetcode/264/264_ugly_number_ii.go
+++ leetcode/264/264_ugly_number_ii.go
@@ -1,36 +1,43 @@
-package _264
-
-import (
-	"go_projects/leetcode_golang_version"
-)
-
 /*
 __author__ = 'lawtech'
 __date__ = '2018/8/19 下午11:22'
 */
 
+package _264
+
+import "math"
+
 func nthUglyNumber(n int) int {
-	if n == 0 {
-		return 0
-	}
-
-	q := []int{1}
+	q := make([]int, n)
+	q[0] = 1
 	i2, i3, i5 := 0, 0, 0
-
-	for len(q) < n {
+	for i := 1; i < len(q); i++ {
 		m2, m3, m5 := q[i2]*2, q[i3]*3, q[i5]*5
-		m, _ := leetcode_golang_version.MinMax([]int{m2, m3, m5})
-		switch m {
-		case 2:
-			i2 += 1
-		case 3:
-			i3 += 1
-		case 5:
-			i5 += 1
-		default:
+		m := Min(m2, m3, m5)
+		q[i] = m
+
+		if m == m2 {
+			i2++
 		}
-		q = append(q, m)
+
+		if m == m3 {
+			i3++
+		}
+
+		if m == m5 {
+			i5++
+		}
 	}
 
-	return q[len(q)-1]
+	return q[n-1]
+}
+
+func Min(args ...int) int {
+	min := math.MaxInt32
+	for _, v := range args {
+		if v < min {
+			min = v
+		}
+	}
+	return min
 }
