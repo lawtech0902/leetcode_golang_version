92f310ab4801d5d655b9875c669403d110d41961
lawtech0902 <584563542@qq.com>
2020-04-20 14:40:59 +0800

    297_serialize_and_deserialize_binary_tree

---
+++ leetcode/297/297_serialize_and_deserialize_binary_tree.go
@@ -0,0 +1,65 @@
+/*
+__author__ = 'lawtech'
+__date__ = '2020/04/20 2:27 下午'
+*/
+
+package _297
+
+import (
+	"strconv"
+	"strings"
+)
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+type Codec struct {
+	res []string
+}
+
+func Constructor() Codec {
+	return Codec{}
+}
+
+// dfs
+// Serializes a tree to a single string.
+func (this *Codec) serialize(root *TreeNode) string {
+	if root == nil {
+		return "#"
+	}
+
+	return strconv.Itoa(root.Val) + "," + this.serialize(root.Left) + "," + this.serialize(root.Right)
+}
+
+// Deserializes your encoded data to tree.
+func (this *Codec) deserialize(data string) *TreeNode {
+	this.res = strings.Split(data, ",")
+	return this.dfsDeserialize()
+}
+
+func (this *Codec) dfsDeserialize() *TreeNode {
+	node := this.res[0]
+	this.res = this.res[1:]
+	if node == "#" {
+		return nil
+	}
+
+	v, _ := strconv.Atoi(node)
+	root := &TreeNode{
+		Val:   v,
+		Left:  this.dfsDeserialize(),
+		Right: this.dfsDeserialize(),
+	}
+
+	return root
+}
+
+/**
+ * Your Codec object will be instantiated and called as such:
+ * obj := Constructor();
+ * data := obj.serialize(root);
+ * ans := obj.deserialize(data);
+ */
