4b5be169522ec7f5a757df9223232b10e3df7491
lawtech0902 <584563542@qq.com>
2020-04-22 13:53:34 +0800

    315_count_of_smaller_numbers_after_self

--- leetcode/315/315_count_of_smaller_numbers_after_self.go
+++ leetcode/315/315_count_of_smaller_numbers_after_self.go
@@ -1,13 +1,13 @@
-package _315
-
 /*
 __author__ = 'lawtech'
 __date__ = '2018/8/20 上午11:46'
 */
 
-func countSmaller(nums []int) []int {
-	res := make([]int, len(nums))
+package _315
 
+// 暴力，也能过
+func countSmaller1(nums []int) []int {
+	res := make([]int, len(nums))
 	for i := 0; i < len(nums); i++ {
 		for j := i + 1; j < len(nums); j++ {
 			if nums[j] < nums[i] {
@@ -17,4 +17,51 @@ func countSmaller(nums []int) []int {
 	}
 
 	return res
+}
+
+// 分治 + 归并
+func countSmaller(nums []int) []int {
+	res := make([]int, len(nums))
+	idxs := make([]int, len(nums))
+	for i := 0; i < len(nums); i++ {
+		idxs[i] = i
+	}
+
+	sort(nums, idxs, append([]int{}, idxs...), res, 0, len(nums)-1)
+	return res
+}
+
+func sort(nums, idxs, sortedIdxs, ans []int, start, end int) {
+	if start >= end {
+		return
+	}
+
+	pivot := start + (end-start)/2
+	sort(nums, idxs, sortedIdxs, ans, start, pivot)
+	sort(nums, idxs, sortedIdxs, ans, pivot+1, end)
+	idx := start
+	i1, i2 := start, pivot+1
+	for i1 <= pivot || i2 <= end {
+		if i1 <= pivot && i2 <= end {
+			if nums[idxs[i1]] > nums[idxs[i2]] {
+				sortedIdxs[idx] = idxs[i2]
+				i2++
+			} else {
+				sortedIdxs[idx] = idxs[i1]
+				ans[idxs[i1]] += i2 - pivot - 1
+				i1++
+			}
+		} else if i1 <= pivot {
+			sortedIdxs[idx] = idxs[i1]
+			ans[idxs[i1]] += end - pivot
+			i1++
+		} else if i2 <= end {
+			sortedIdxs[idx] = idxs[i2]
+			i2++
+		}
+
+		idx++
+	}
+
+	copy(idxs[start:end+1], sortedIdxs[start:end+1])
 }
