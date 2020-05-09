93299dd6043d9b5714504fa0f1ac0ff1bdfe8fdf
lawtech0902 <584563542@qq.com>
2020-05-09 10:08:11 +0800

    377_combination_sum_iv

--- leetcode/377/377_combination_sum_iv.go
+++ leetcode/377/377_combination_sum_iv.go
@@ -1,19 +1,18 @@
-package _377
-
 /*
 __author__ = 'lawtech'
 __date__ = '2018/8/21 下午8:28'
 */
 
+package _377
+
 // dp[x]表示生成数字x的所有可能的组合方式的个数。
 func combinationSum4(nums []int, target int) int {
 	dp := make([]int, target+1)
 	dp[0] = 1
-
-	for i := 0; i <= target; i++ {
-		for _, num := range nums {
-			if i+num <= target {
-				dp[i+num] += dp[i]
+	for i := 1; i <= target; i++ {
+		for _, v := range nums {
+			if i-v >= 0 {
+				dp[i] += dp[i-v]
 			}
 		}
 	}
