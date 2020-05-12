2eff94ade65a396ef5e04a385ff738a32cd779a8
lawtech0902 <584563542@qq.com>
2020-05-12 13:25:04 +0800

    392_is_subsequence

--- leetcode/392/392_is_subsequence.go
+++ leetcode/392/392_is_subsequence.go
@@ -1,17 +1,17 @@
-package _392
-
 /*
 __author__ = 'lawtech'
 __date__ = '2018/8/20 下午11:09'
 */
 
+package _392
+
 func isSubsequence(s string, t string) bool {
 	i, j := 0, 0
-
 	for i < len(s) && j < len(t) {
 		if s[i] == t[j] {
 			i++
 		}
+
 		j++
 	}
 
