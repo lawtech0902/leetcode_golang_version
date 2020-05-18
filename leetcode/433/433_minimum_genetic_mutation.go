68f696401fdc92ff9bfcb085d5d120f9c1f448f5
lawtech0902 <584563542@qq.com>
2020-05-18 17:13:59 +0800

    433_minimum_genetic_mutation

---
+++ leetcode/433/433_minimum_genetic_mutation.go
@@ -0,0 +1,46 @@
+/*
+__author__ = 'lawtech'
+__date__ = '2020/05/18 4:08 下午'
+*/
+
+package _433
+
+// bfs
+func minMutation(start string, end string, bank []string) int {
+	used := make([]int, len(bank))
+	stack := []string{start}
+	nums := 0
+	for len(stack) != 0 {
+		nums++
+		level := len(stack)
+		for i := 0; i < level; i++ {
+			for k, v := range bank {
+				if v != start && used[k] == 0 {
+					diff := 0
+					for m := 0; m < 8; m++ {
+						if v[m] != stack[i][m] {
+							diff++
+						}
+
+						if diff >= 2 {
+							break
+						}
+					}
+
+					if diff == 1 {
+						used[k] = nums
+						if bank[k] == end {
+							return nums
+						}
+
+						stack = append(stack, bank[k])
+					}
+				}
+			}
+		}
+
+		stack = stack[level:]
+	}
+
+	return -1
+}
