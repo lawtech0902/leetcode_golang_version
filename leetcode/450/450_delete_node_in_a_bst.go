a8d9737711828c9fafe2d39beb3976b7bf30116c
lawtech0902 <584563542@qq.com>
2020-05-21 14:20:30 +0800

    450_delete_node_in_a_bst

---
+++ leetcode/450/450_delete_node_in_a_bst.go
@@ -0,0 +1,54 @@
+/*
+__author__ = 'lawtech'
+__date__ = '2020/05/21 1:48 下午'
+*/
+
+package _450
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func deleteNode(root *TreeNode, key int) *TreeNode {
+	if root == nil {
+		return nil
+	}
+
+	if key > root.Val {
+		root.Right = deleteNode(root.Right, key)
+	} else if key < root.Val {
+		root.Left = deleteNode(root.Left, key)
+	} else {
+		if root.Left == nil && root.Right == nil {
+			root = nil
+		} else if root.Right != nil {
+			root.Val = successor(root)
+			root.Right = deleteNode(root.Right, root.Val)
+		} else {
+			root.Val = predecessor(root)
+			root.Left = deleteNode(root.Left, root.Val)
+		}
+	}
+
+	return root
+}
+
+func successor(root *TreeNode) int {
+	root = root.Right
+	for root.Left != nil {
+		root = root.Left
+	}
+
+	return root.Val
+}
+
+func predecessor(root *TreeNode) int {
+	root = root.Left
+	for root.Left != nil {
+		root = root.Right
+	}
+
+	return root.Val
+}
