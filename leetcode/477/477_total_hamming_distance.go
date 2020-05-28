39633f5ac6d6a8caad6bd2d9c492d64d08ec5290
lawtech0902 <584563542@qq.com>
2020-05-28 10:43:55 +0800

    477_total_hamming_distance

--- leetcode/477/477_total_hamming_distance.go
+++ leetcode/477/477_total_hamming_distance.go
@@ -5,6 +5,7 @@ __date__ = '2020/05/28 10:25 上午'
 
 package _477
 
+// 考虑数组中每个数二进制的第 i 位，假设一共有 t 个 0 和 n - t 个 1，那么显然在第 i 位的汉明距离的总和为 t * (n - t)。
 func totalHammingDistance(nums []int) int {
 	res, n := 0, len(nums)
 	for i := 0; i < 32; i++ {
