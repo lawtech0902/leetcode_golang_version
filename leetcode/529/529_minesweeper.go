6b225e0f7735a433c55a5add6d6683c5e262003f
lawtech0902 <584563542@qq.com>
2020-06-12 10:55:56 +0800

    529_minesweeper

---
+++ leetcode/529/529_minesweeper.go
@@ -0,0 +1,44 @@
+/*
+__author__ = 'lawtech'
+__date__ = '2020/06/12 9:51 上午'
+*/
+
+package _529
+
+var d = [8][2]int{{1, 0}, {-1, 0}, {0, 1}, {0, -1}, {1, 1}, {1, -1}, {-1, 1}, {-1, -1}}
+
+func updateBoard(board [][]byte, click []int) [][]byte {
+	a, b := click[0], click[1]
+	if board[a][b] == 'M' {
+		board[a][b] = 'X'
+	} else if board[a][b] == 'E' {
+		m, n := len(board), len(board[0])
+
+		var f func(int, int)
+		f = func(i, j int) {
+			c := byte('0')
+			for _, di := range d {
+				x, y := i+di[0], j+di[1]
+				if 0 <= x && x < m && 0 <= y && y < n && board[x][y] == 'M' {
+					c++
+				}
+			}
+
+			if c > '0' {
+				board[i][j] = c
+			} else {
+				board[i][j] = 'B'
+				for _, di := range d {
+					x, y := i+di[0], j+di[1]
+					if 0 <= x && x < m && 0 <= y && y < n && board[x][y] == 'E' {
+						f(x, y)
+					}
+				}
+			}
+		}
+
+		f(a, b)
+	}
+
+	return board
+}
