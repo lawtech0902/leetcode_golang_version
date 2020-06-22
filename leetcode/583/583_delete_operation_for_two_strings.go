fa76d8a6fecf1ff4f12db09016ac613524119c66
lawtech0902 <584563542@qq.com>
2020-06-22 13:48:33 +0800

    583_delete_operation_for_two_strings

--- leetcode/583/583_delete_operation_for_two_strings.go
+++ leetcode/583/583_delete_operation_for_two_strings.go
@@ -1,11 +1,11 @@
+/*
+ * @Description:
+ * @Author: lawtech
+ * @Date: 2020-01-03 16:19:42
+ */
 package _583
 
 import "math"
-
-/*
-__author__ = 'lawtech'
-__date__ = '2018/8/21 下午11:05'
-*/
 
 // 转化为求两个字符串的最长公共子序列问题
 func minDistance(word1 string, word2 string) int {
@@ -14,9 +14,8 @@ func minDistance(word1 string, word2 string) int {
 
 func lengthOfLCS(word1, word2 string) int {
 	n1, n2 := len(word1), len(word2)
-
 	dp := make([][]int, n1+1)
-	for i := 0; i <= n1; i++ {
+	for i := range dp {
 		dp[i] = make([]int, n2+1)
 	}
 
