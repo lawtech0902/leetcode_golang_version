3b233e6c740fa8c565d8927fe14d8e7f20d21c13
robin-luo <robin_luo@trendmicro.com>
2020-07-02 09:51:34 +0800

    633_sum_of_square_numbers

--- leetcode/633/633_sum_of_square_numbers.go
+++ leetcode/633/633_sum_of_square_numbers.go
@@ -1,3 +1,8 @@
+/*
+ * @Description:
+ * @Author: lawtech
+ * @Date: 2020-01-03 16:19:42
+ */
 package _633
 
 import "math"
@@ -7,6 +12,7 @@ __author__ = 'lawtech'
 __date__ = '2018/8/24 下午6:03'
 */
 
+// 双指针
 func judgeSquareSum(c int) bool {
 	i, j := 0, int(math.Sqrt(float64(c)))
 
@@ -22,4 +28,23 @@ func judgeSquareSum(c int) bool {
 	}
 
 	return false
+}
+
+// 费马平方和定理：一个非负整数 cc 能够表示为两个整数的平方和，当且仅当 cc 的所有形如 4k+34k+3 的质因子的幂次均为偶数。
+func judgeSquareSum1(c int) bool {
+	for i := 2; i*i <= c; i++ {
+		count := 0
+		if c%i == 0 {
+			for c%i == 0 {
+				count++
+				c /= i
+			}
+
+			if i%4 == 3 && count%2 != 0 {
+				return false
+			}
+		}
+	}
+
+	return c%4 != 3
 }
