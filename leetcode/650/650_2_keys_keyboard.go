e56bf980546ca2346bfe5d647c50b614699b6951
robin-luo <robin_luo@trendmicro.com>
2020-07-06 13:48:55 +0800

    650_2_keys_keyboard

--- leetcode/650/650_2_keys_keyboard.go
+++ leetcode/650/650_2_keys_keyboard.go
@@ -1,12 +1,12 @@
+/*
+ * @Description:
+ * @Author: lawtech
+ * @Date: 2020-01-03 16:19:42
+ */
+
 package _650
 
-import "math"
-
-/*
-__author__ = 'lawtech'
-__date__ = '2018/8/21 下午11:45'
-*/
-
+// dp
 func minSteps(n int) int {
 	dp := make([]int, n+1)
 
@@ -14,10 +14,33 @@ func minSteps(n int) int {
 		dp[i] = i
 		for j := i - 1; j > 1; j-- {
 			if i%j == 0 {
-				dp[i] = int(math.Min(float64(dp[i]), float64(dp[j]+i/j)))
+				dp[i] = min(dp[i], dp[j]+i/j)
 			}
 		}
 	}
 
 	return dp[n]
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+
+	return b
+}
+
+// 素数分解
+func minSteps1(n int) int {
+	res, d := 0, 2
+	for n > 1 {
+		for n%d == 0 {
+			res += d
+			n /= d
+		}
+
+		d++
+	}
+
+	return res
 }
