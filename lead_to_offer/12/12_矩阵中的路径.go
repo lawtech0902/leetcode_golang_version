5ffd7ecb1c579e065d2686cd27dad17b06bb05f7
robin-luo <robin_luo@trendmicro.com>
2020-07-28 11:22:49 +0800

    12_矩阵中的路径

---
+++ lead_to_offer/12/12_矩阵中的路径.go
@@ -0,0 +1,35 @@
+/*
+__author__ = 'lawtech'
+__date__ = '2020/07/28 10:48 上午'
+*/
+
+package _12
+
+func exist(board [][]byte, word string) bool {
+	for i := 0; i < len(board); i++ {
+		for j := 0; j < len(board[0]); j++ {
+			if dfs(board, word, i, j, 0) {
+				return true
+			}
+		}
+	}
+
+	return false
+}
+
+func dfs(board [][]byte, word string, i, j, k int) bool {
+	if i >= len(board) || i < 0 || j >= len(board[0]) || j < 0 || board[i][j] != word[k] {
+		return false
+	}
+
+	if k == len(word)-1 {
+		return true
+	}
+
+	tmp := board[i][j]
+	board[i][j] = '/'
+	res := dfs(board, word, i+1, j, k+1) || dfs(board, word, i-1, j, k+1) ||
+		dfs(board, word, i, j+1, k+1) || dfs(board, word, i, j-1, k+1)
+	board[i][j] = tmp
+	return res
+}
