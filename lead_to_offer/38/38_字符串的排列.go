0b677b7c20a0098adc2062a42cac05679fbb65c8
robin-luo <robin_luo@trendmicro.com>
2020-08-03 13:42:36 +0800

    38_字符串的排列

---
+++ lead_to_offer/38/38_字符串的排列.go
@@ -0,0 +1,30 @@
+/*
+__author__ = 'lawtech'
+__date__ = '2020/08/03 11:14 上午'
+*/
+
+package _38
+
+func permutation(s string) []string {
+	var res []string
+	helper([]byte(s), 0, &res)
+	return res
+}
+
+func helper(s []byte, start int, res *[]string) {
+	if start == len(s) {
+		*res = append(*res, string(s))
+	}
+
+	m := make(map[byte]int)
+	for i := start; i < len(s); i++ {
+		if _, ok := m[s[i]]; ok {
+			continue
+		}
+
+		s[i], s[start] = s[start], s[i]
+		helper(s, start+1, res)
+		s[i], s[start] = s[start], s[i]
+		m[s[i]] = 1
+	}
+}
