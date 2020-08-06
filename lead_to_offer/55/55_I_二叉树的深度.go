6ab7e2fb962aab6afb5fa49ab565f2302c640f98
robin-luo <robin_luo@trendmicro.com>
2020-08-06 14:26:56 +0800

    55_I_二叉树的深度

---
+++ lead_to_offer/55/55_I_二叉树的深度.go
@@ -0,0 +1,33 @@
+/*
+__author__ = 'lawtech'
+__date__ = '2020/08/06 2:02 下午'
+*/
+
+package _55
+
+import "math"
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func maxDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+
+	return 1 + max(maxDepth(root.Left), maxDepth(root.Right))
+}
+
+func max(values ...int) int {
+	maxValue := math.MinInt32
+	for _, v := range values {
+		if v > maxValue {
+			maxValue = v
+		}
+	}
+
+	return maxValue
+}
