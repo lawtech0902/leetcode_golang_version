fac7d27f77b0bcec3f5ecf9a25b1e456aa1f853b
robin-luo <robin_luo@trendmicro.com>
2020-08-12 10:29:21 +0800

    68_II_二叉树的最近公共祖先

---
+++ lead_to_offer/68/68_II_二叉树的最近公共祖先.go
@@ -0,0 +1,32 @@
+/*
+__author__ = 'lawtech'
+__date__ = '2020/08/12 10:09 上午'
+*/
+
+package _68
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+	if root == nil {
+		return nil
+	}
+
+	if root == p || root == q {
+		return root
+	}
+
+	left := lowestCommonAncestor(root.Left, p, q)
+	right := lowestCommonAncestor(root.Right, p, q)
+	if left != nil && right != nil {
+		return root
+	} else if left == nil {
+		return right
+	} else {
+		return left
+	}
+}
