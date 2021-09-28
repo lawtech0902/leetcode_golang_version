e4f6adbe6ae86178a5445980507bfef4f612b6d7
robin-luo <robin_luo@trendmicro.com>
2021-09-28 15:11:42 +0800

    剑指 Offer II 022. 链表中环的入口节点

---
+++ lead_to_offer_ii/22.go
@@ -0,0 +1,21 @@
+package lead_to_offer_ii
+
+// 快慢指针
+func detectCycle(head *ListNode) *ListNode {
+	slow, fast := head, head
+	for fast != nil && fast.Next != nil {
+		slow = slow.Next
+		fast = fast.Next.Next
+		if slow == fast {
+			fast = head
+			for slow != fast {
+				slow = slow.Next
+				fast = fast.Next
+			}
+
+			return slow
+		}
+	}
+
+	return nil
+}
